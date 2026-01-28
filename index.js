e4414c680701128e3ad5950062195675367658c9
Davit Vekua <davitvekua23@gmail.com>
2026-01-28 13:29:51 +0100

    Addierer und Reloader fertig

--- index.js
+++ index.js
@@ -3,85 +3,68 @@ var element = document.getElementById("sum");
 
 function a1() {
   sum += 3.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a2() {
   sum += 2.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a3() {
   sum += 4;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a4() {
   sum += 2;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a5() {
   sum += 5.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a6() {
   sum += 12.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a7() {
   sum += 6;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a8() {
   sum += 4.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a9() {
   sum += 8;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a10() {
   sum += 3.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a11() {
   sum += 4.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a12() {
   sum += 3;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a13() {
   sum += 3;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a14() {
   sum += 2.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a15() {
   sum += 7;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
 function a16() {
   sum += 6.5;
-  element.innerHTML = sum;
+  element.innerHTML = sum + " €";
 }
-function b() {
-  console.log(sum);
+function reloadSite() {
+  location.reload();
 }
-
-// const a1 = 3.5;
-// const a2 = 2.5;
-// const a3 = 4.0;
-// const a4 = 2.0;
-// const a5 = 5.5;
-// const a6 = 12.5;
-// const a7 = 6.0;
-// const a8 = 4.5;
-// const a9 = 8.0;
-// const a10 = 3.5;
-// const a11 = 4.5;
-// const a12 = 3.0;
-// const a13 = 3.0;
-// const a14 = 2.5;
-// const a15 = 7.0;
-// const a16 = 6.5;
